a2a98a2f6db5efb20f72881e892321b5da8bbadd
adi forkasiewicz <adiforka@gmail.com>
2020-12-29 17:51:52 +0100

    added a new todo component and used refs to get user input from form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import TodoList from "./components/TodoList"
+import NewTodo from "./components/NewTodo"
 
 const App: React.FC = () => {
   const todos = [
@@ -8,6 +9,7 @@ const App: React.FC = () => {
   ]
   return (
     <div className="App">
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   )
